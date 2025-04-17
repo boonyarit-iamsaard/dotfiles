9dd7840cd377791b22e0b397c962bdd27abdb08c
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-04-17 20:13:43 +0700

    chore: sync

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,24 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    past = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_endbled = 0,
+  }
+end
+
+vim.g.lazyvim_php_lsp = "intelephense"
+
+local opt = vim.opt
+
+opt.relativenumber = false
