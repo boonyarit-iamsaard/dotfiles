3c22e446224140066950f0690e9a1b29f66bb847
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-06-07 21:51:17 +0700

    chore: sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -66,6 +66,36 @@ return {
     end,
   },
 
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   config = function()
+  --     require("kanagawa").setup({
+  --       compile = false,
+  --       undercurl = true,
+  --       commentStyle = { italic = true },
+  --       functionStyle = {},
+  --       keywordStyle = { italic = true },
+  --       statementStyle = { bold = true },
+  --       typeStyle = {},
+  --       transparent = true,
+  --       dimInactive = false,
+  --       terminalColors = true,
+  --       colors = {
+  --         palette = {},
+  --         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --       },
+  --       overrides = function(_)
+  --         return {}
+  --       end,
+  --       theme = "wave",
+  --       background = {
+  --         dark = "wave",
+  --         light = "lotus",
+  --       },
+  --     })
+  --   end,
+  -- },
+
   {
     "catppuccin/nvim",
     name = "catppuccin",
