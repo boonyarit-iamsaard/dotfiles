0307bbd1a6c394f3c8050dce3d973fc29b68cace
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-09-02 15:04:45 +0700

    chore: sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -93,23 +93,23 @@ return {
     end,
   },
 
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_enable_italic = true
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
-
   -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin",
-  --   },
+  --   "sainnhe/gruvbox-material",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.gruvbox_material_background = "hard"
+  --     vim.g.gruvbox_material_enable_italic = true
+  --     vim.cmd.colorscheme("gruvbox-material")
+  --   end,
   -- },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
