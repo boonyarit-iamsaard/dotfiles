2e3e451adcfafec40c053c6a36276d896b4b3991
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-04-23 19:40:57 +0700

    chore: sync

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -103,8 +103,8 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "catppuccin-mocha",
-      colorscheme = "vscode",
+      colorscheme = "catppuccin-mocha",
+      -- colorscheme = "vscode",
     },
   },
 }
