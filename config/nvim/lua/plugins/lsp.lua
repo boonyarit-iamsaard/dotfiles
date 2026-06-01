85c77b6764fb26424d933a096d7f67c40ddae632
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-06-01 20:58:21 +0700

    chore: sync

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,14 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = {
-        enabled = false,
-      },
-    },
+    opts = function(_, opts)
+      opts.inlay_hints.enabled = false
+      opts.servers["*"].capabilities = vim.tbl_deep_extend(
+        "force",
+        vim.lsp.protocol.make_client_capabilities(),
+        opts.servers["*"].capabilities or {}
+      )
+      opts.setup.gopls = function() end
+    end,
   },
 }
