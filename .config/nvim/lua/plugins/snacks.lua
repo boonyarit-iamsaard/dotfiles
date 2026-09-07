34733d52dd32cea24ed252210f41ad73854c1a16
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-09-07 18:01:45 +0700

    chore: sync

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -30,27 +30,27 @@ return {
           },
         },
       },
-      lazygit = {
-        config = {
-          gui = {
-            theme = {
-              activeBorderColor = { "#3994bc", "bold" }, -- tab.activeBorderTop
-              inactiveBorderColor = { "#8c8c8c" }, -- descriptionForeground
-              optionsTextColor = { "#48a0c7" }, -- textLink.foreground
-              selectedLineBgColor = { "#276782" }, -- editor.selectionBackground, opaque base
-              inactiveViewSelectedLineBgColor = { "#2c2d2e" }, -- list.inactiveSelectionBackground
-              cherryPickedCommitBgColor = { "#37373d" }, -- tab.selectedBackground
-              cherryPickedCommitFgColor = { "#e5ba7d" }, -- gitDecoration.modifiedResourceForeground
-              markedBaseCommitBgColor = { "#37373d" }, -- tab.selectedBackground
-              markedBaseCommitFgColor = { "#57a3f8" }, -- charts.blue
-              unstagedChangesColor = { "#f48771" }, -- gitDecoration.deletedResourceForeground
-              defaultFgColor = { "#cccccc" }, -- terminal.foreground
-              searchingActiveBorderColor = { "#e5ba7d" }, -- gitDecoration.modifiedResourceForeground
-            },
-            authorColors = { ['"*"'] = "#57a3f8" }, -- charts.blue
-          },
-        },
-      },
+      -- lazygit = {
+      --   config = {
+      --     gui = {
+      --       theme = {
+      --         activeBorderColor = { "#3994bc", "bold" }, -- tab.activeBorderTop
+      --         inactiveBorderColor = { "#8c8c8c" }, -- descriptionForeground
+      --         optionsTextColor = { "#48a0c7" }, -- textLink.foreground
+      --         selectedLineBgColor = { "#276782" }, -- editor.selectionBackground, opaque base
+      --         inactiveViewSelectedLineBgColor = { "#2c2d2e" }, -- list.inactiveSelectionBackground
+      --         cherryPickedCommitBgColor = { "#37373d" }, -- tab.selectedBackground
+      --         cherryPickedCommitFgColor = { "#e5ba7d" }, -- gitDecoration.modifiedResourceForeground
+      --         markedBaseCommitBgColor = { "#37373d" }, -- tab.selectedBackground
+      --         markedBaseCommitFgColor = { "#57a3f8" }, -- charts.blue
+      --         unstagedChangesColor = { "#f48771" }, -- gitDecoration.deletedResourceForeground
+      --         defaultFgColor = { "#cccccc" }, -- terminal.foreground
+      --         searchingActiveBorderColor = { "#e5ba7d" }, -- gitDecoration.modifiedResourceForeground
+      --       },
+      --       authorColors = { ['"*"'] = "#57a3f8" }, -- charts.blue
+      --     },
+      --   },
+      -- },
     },
   },
 }
